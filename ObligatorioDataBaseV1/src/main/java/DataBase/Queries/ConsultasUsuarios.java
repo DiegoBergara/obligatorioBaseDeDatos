b439c92d9d45992c7bd908f3415b04f3767a506e
Faivel <faivel.f.s@gmail.com>
2019-11-01 13:45:48 -0300

    Se agregan Forms de logeo y registro de usuarios. Se agrega consulta de usuario por email y password

--- ObligatorioDataBaseV1/src/main/java/DataBase/Queries/ConsultasUsuarios.java
+++ ObligatorioDataBaseV1/src/main/java/DataBase/Queries/ConsultasUsuarios.java
@@ -10,7 +10,10 @@ import Data.Classes.Usuario;
  import DataBase.Connection.ConnectionManager;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 /**
  *
  * @author USER
@@ -24,7 +27,7 @@ public class ConsultasUsuarios {
         Connection connection = ConnectionManager.getConnection();
         try {
 
-            PreparedStatement statement = connection.prepareStatement("insert into usuarios values(?,?,?,?,?,?,?)");
+            PreparedStatement statement = connection.prepareStatement("insert into usuarios values(?,?,?,?,?,?,?,?)");
             statement.setString(1, user.mail);
             statement.setString(2, user.password);
             statement.setString(3, user.nombre);
@@ -32,6 +35,7 @@ public class ConsultasUsuarios {
             statement.setInt(5, user.valoracion);
             statement.setBoolean(6, user.conductor);
             statement.setString(7, user.vehiculo);
+            statement.setInt(8, 1);
             statement.executeUpdate();
 
             return true;
@@ -39,6 +43,42 @@ public class ConsultasUsuarios {
         } catch (SQLException sqle) {
             System.out.println("Error: " + sqle);
             return false;
+        } finally {
+            ConnectionManager.closeConnection(connection);
+        }
+    }
+    
+    public Usuario getUser(String mail, String pass) {
+        Connection connection = ConnectionManager.getConnection();
+        List<Usuario> users = new LinkedList<Usuario>();
+        try {
+
+            PreparedStatement statement = connection.prepareStatement("Select * from usuarios Where mail=? and contraseña=?");
+            statement.setString(1, mail);
+            statement.setString(2, pass);
+            
+            ResultSet rs = statement.executeQuery();
+            while ( rs.next() )
+            {
+                users.add(new Usuario(rs.getString("mail"),
+                        rs.getString("contraseña"),
+                        rs.getString("nombre"), 
+                        rs.getString("apellido"), 
+                        rs.getBoolean("esConductor"),
+                        rs.getString("vehiculo")));
+            }
+            
+            rs.close();
+            statement.close();
+            if(users.size() == 1){
+                return users.get(0);
+            }
+            return null;
+            
+
+        } catch (SQLException sqle) {
+            System.out.println("Error: " + sqle);
+            return null;
         } finally {
             ConnectionManager.closeConnection(connection);
         }
